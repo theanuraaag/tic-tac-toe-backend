00d8cf1aeb82be79f156c2d78b9281e722b85f19
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2025-04-14 21:35:48 +0530

    Update CORS to allow frontend Netlify URL

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  cors: "http://localhost:5174/",
+  cors: "https://tic-tac-toe-react-socketio-game.netlify.app/",
 });
 
 const allUsers = {};
